fbfbe7b182df4bc553522930c8c9af771ca78232
WGwg <956910931@qq.com>
2018-01-27 11:10:31 +0800

    添加了dataTable+pagehelper的行内编辑功能，整合物理分页

--- src/main/resources/static/js/editable-table.js
+++ src/main/resources/static/js/editable-table.js
@@ -18,42 +18,64 @@ var EditableTable = function () {
             function editRow(oTable, nRow) {
                 var aData = oTable.fnGetData(nRow);
                 var jqTds = $('>td', nRow);
-                jqTds[0].innerHTML = '<input type="text" class="form-control small" value="' + aData[0] + '">';
+                /*jqTds[0].innerHTML = '<input type="text" class="form-control small" value="' + aData[0] + '">';
                 jqTds[1].innerHTML = '<input type="text" class="form-control small" value="' + aData[1] + '">';
                 jqTds[2].innerHTML = '<input type="text" class="form-control small" value="' + aData[2] + '">';
                 jqTds[3].innerHTML = '<input type="text" class="form-control small" value="' + aData[3] + '">';
                 jqTds[4].innerHTML = '<a class="edit" href="">Save</a>';
-                jqTds[5].innerHTML = '<a class="cancel" href="">Cancel</a>';
+                jqTds[5].innerHTML = '<a class="cancel" href="">Cancel</a>';*/
+                for(var i=0;i<jqTds.length-2;i++){
+                		jqTds[i].innerHTML = '<input type="text" class="form-control small" value="' + aData[i] + '">';
+                }
+                jqTds[jqTds.length-2].innerHTML = '<a class="edit" href="">Save</a>';
+                jqTds[jqTds.length-1].innerHTML = '<a class="cancel" href="">Cancel</a>';
             }
 
             function saveRow(oTable, nRow) {
-                var jqInputs = $('input', nRow);
-                oTable.fnUpdate(jqInputs[0].value, nRow, 0, false);
+                var jqInputs = $('input', nRow);  //输入框长度  选择器 input
+                /*oTable.fnUpdate(jqInputs[0].value, nRow, 0, false);
                 oTable.fnUpdate(jqInputs[1].value, nRow, 1, false);
                 oTable.fnUpdate(jqInputs[2].value, nRow, 2, false);
                 oTable.fnUpdate(jqInputs[3].value, nRow, 3, false);
-                oTable.fnUpdate('<a class="edit" href="">Edit</a>', nRow, 4, false);
-                oTable.fnUpdate('<a class="delete" href="">Delete</a>', nRow, 5, false);
+                oTable.fnUpdate(jqInputs[4].value, nRow, 4, false);
+                oTable.fnUpdate(jqInputs[5].value, nRow, 5, false);
+                oTable.fnUpdate(jqInputs[6].value, nRow, 6, false);
+                oTable.fnUpdate('<a class="edit" href="">Edit</a>', nRow, 7, false);
+                oTable.fnUpdate('<a class="delete" href="">Delete</a>', nRow, 8, false);*/
+                
+                for(var i=0;i<jqInputs.length;i++){
+               	 oTable.fnUpdate(jqInputs[i].value, nRow, i, false);
+               }
+               oTable.fnUpdate('<a class="edit" href="">Edit</a>', nRow, jqInputs.length, false);
+               oTable.fnUpdate('<a class="delete" href="">Delete</a>', nRow, jqInputs.length+1, false);
+                
                 oTable.fnDraw();
+                
             }
 
             function cancelEditRow(oTable, nRow) {
                 var jqInputs = $('input', nRow);
-                oTable.fnUpdate(jqInputs[0].value, nRow, 0, false);
+              /*  oTable.fnUpdate(jqInputs[0].value, nRow, 0, false);
                 oTable.fnUpdate(jqInputs[1].value, nRow, 1, false);
                 oTable.fnUpdate(jqInputs[2].value, nRow, 2, false);
                 oTable.fnUpdate(jqInputs[3].value, nRow, 3, false);
-                oTable.fnUpdate('<a class="edit" href="">Edit</a>', nRow, 4, false);
+                oTable.fnUpdate('<a class="edit" href="">Edit</a>', nRow, 4, false);*/
+                
+                for(var i=0;i<jqInputs.length;i++){
+                  	 oTable.fnUpdate(jqInputs[i].value, nRow, i, false);
+                  }
+                oTable.fnUpdate('<a class="edit" href="">Edit</a>', nRow, jqInputs.length, false);
+                
                 oTable.fnDraw();
             }
 
-            var oTable = $('#editable-sample').dataTable({
-                "aLengthMenu": [
+           var oTable = $('#editable-sample').dataTable({
+        	   /* "aLengthMenu": [
                     [5, 15, 20, -1],
                     [5, 15, 20, "All"] // change per page values here
                 ],
                 // set the initial value
-                "iDisplayLength": 5,
+               "iDisplayLength": 5,
                 "sDom": "<'row'<'col-lg-6'l><'col-lg-6'f>r>t<'row'<'col-lg-6'i><'col-lg-6'p>>",
                 "sPaginationType": "bootstrap",
                 "oLanguage": {
@@ -67,17 +89,21 @@ var EditableTable = function () {
                         'bSortable': false,
                         'aTargets': [0]
                     }
-                ]
+                ]*/
+            	bFilter: false,    //去掉搜索框
+            	 bLengthChange: false,   //去掉每页显示多少条数据方法
+                bPaginate: false,//显示（使用）分页器
+                bInfo:false,
             });
-
-            jQuery('#editable-sample_wrapper .dataTables_filter input').addClass("form-control medium"); // modify table search input
-            jQuery('#editable-sample_wrapper .dataTables_length select').addClass("form-control xsmall"); // modify table per page dropdown
+            
+         /*   jQuery('#editable-sample_wrapper .dataTables_filter input').addClass("form-control medium"); // modify table search input
+            jQuery('#editable-sample_wrapper .dataTables_length select').addClass("form-control xsmall"); // modify table per page dropdown*/
 
             var nEditing = null;
 
             $('#editable-sample_new').click(function (e) {
                 e.preventDefault();
-                var aiNew = oTable.fnAddData(['', '', '', '',
+                var aiNew = oTable.fnAddData(['', '', '', '','','','',
                         '<a class="edit" href="">Edit</a>', '<a class="cancel" data-mode="new" href="">Cancel</a>'
                 ]);
                 var nRow = oTable.fnGetNodes(aiNew[0]);
@@ -88,24 +114,26 @@ var EditableTable = function () {
             $('#editable-sample a.delete').live('click', function (e) {
                 e.preventDefault();
 
-                if (confirm("Are you sure to delete this row ?") == false) {
+                if (confirm("确定删除吗 ?") == false) {
                     return;
                 }
 
                 var nRow = $(this).parents('tr')[0];
                 oTable.fnDeleteRow(nRow);
-                alert("Deleted! Do not forget to do some ajax to sync with backend :)");
+                alert("删除成功！");
             });
 
             $('#editable-sample a.cancel').live('click', function (e) {
                 e.preventDefault();
-                if ($(this).attr("data-mode") == "new") {
+                /*if ($(this).attr("data-mode") == "new") {
                     var nRow = $(this).parents('tr')[0];
                     oTable.fnDeleteRow(nRow);
                 } else {
                     restoreRow(oTable, nEditing);
                     nEditing = null;
-                }
+                }*/
+                var nRow = $(this).parents('tr')[0];
+                oTable.fnDeleteRow(nRow);
             });
 
             $('#editable-sample a.edit').live('click', function (e) {
@@ -123,7 +151,7 @@ var EditableTable = function () {
                     /* Editing this row and want to save it */
                     saveRow(oTable, nEditing);
                     nEditing = null;
-                    alert("Updated! Do not forget to do some ajax to sync with backend :)");
+                    alert("更新成功！");
                 } else {
                     /* No edit in progress - let's start one */
                     editRow(oTable, nRow);
